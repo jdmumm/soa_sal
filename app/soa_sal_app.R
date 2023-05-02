d89202baba9efdc1f0bf3acd86433508391bee82
Mumm <joshua.mumm@dfg.alaska.local>
2023-05-01 17:42:55 -0800

    init

---
+++ app/soa_sal_app.R
@@ -0,0 +1,94 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+
+read.csv("sal_allDepts_22_10K.csv") -> raw
+raw %>% mutate (Class = str_remove(Title, "\\s*\\d+$")) -> all -> data
+
+read.csv("salSched_c23_220701_ann_long.csv") -> sal_long
+
+ui <- navbarPage(
+  title = "State of Alaska Salaries, 2022",
+  tabPanel("Salary by Title",
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput("employers", "Select Department(s)", 
+                         choices = c("All", unique(data$Employer)),
+                         selected = "Alaska Department of Fish and Game"),
+      selectizeInput("classes", "Select Job Class(es)", 
+                     choices = c("All", unique(arrange(data, Class)$Class)),
+                     multiple = TRUE, selected = (c
+                                                  ('Gis Analyst','Biometrician','Research Analyst', 'Analyst/Programmer',
+                                                    'Fish and Game Coordinator', 'Program Coordinator', 'Project Assistant',
+                                                    'Fisheries Scientist', 'Fishery Biologist', 'Wildlife Biologist',
+                                                    'Wildlife Scientist', 'Data Processing Manager', 'Fish & Game Coordinator', 
+                                                    'Fish & Game Coordinator', 'Commissioner', 'Division Director', 'Deputy Director',
+                                                    'Habitat Biologist', 'Natural Resource Manager', 'Division Operations Manager'))
+      ), 
+      width = 3,
+      helpText("Warning: Selecting 'All' for both Department and Class may take several minutes to process."),
+      HTML("<a href='https://www.openthebooks.com/alaska-state-employees/'> Data Source </a>"),
+    ),
+    
+    mainPanel(
+      plotlyOutput("plot", height = "800px", width = "100%")
+    )
+  )),
+  
+    tabPanel("Salary by Range and Step",
+            plotlyOutput("plot2", height = "800px", width = "100%"), 
+            helpText("Step advancement is annual until step J, at which point it becomes biennial."),
+            HTML("<a href='https://doa.alaska.gov/dof/payroll/sal_sched.html'> Data Source </a>")
+          )
+        )
+
+server <- function(input, output, session) {
+  # Server logic for Tab 1
+    # Create the scatter plot
+    output$plot <- renderPlotly({
+      if ("All" %in% input$employers && "All" %in% input$classes) {data -> data_subset}
+      if ("All" %in% input$employers && !("All" %in% input$classes)) {
+        data %>% filter(Class %in% input$classes) -> data_subset}
+      if (!("All" %in% input$employers) && "All" %in% input$classes){
+        data %>% filter(Employer %in% input$employers) -> data_subset}
+      if (!("All" %in% input$employers) && !("All" %in% input$classes)){
+        data %>% filter(Employer %in% input$employers & Class %in% input$classes) -> data_subset}
+      
+      
+      p <- ggplot(data_subset, aes(x=Title, y=wages)) +
+        geom_point(alpha = .2)
+      
+      # Add tooltip text
+      p <- p + aes(text = paste(Name, "<br>",
+                                Title, "<br>",
+                                Employer, "<br>",
+                                Annual.Wages))
+      
+      # Customize the plot
+      p <- p + labs(x = "Title",
+                    y = "Annual Wages (thousands)") +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+        scale_y_continuous(labels = scales::comma_format(scale = 1e-3))
+      
+      # Convert the plot to interactive mode using plotly
+      ggplotly(p, tooltip = "text")
+    })
+  
+  # Server logic for Tab 2
+    output$plot2 <- renderPlotly({
+    ggplot(sal_long, aes(x = Step, y = Salary/1000, group = Range, color = as.factor(Range))) +
+      geom_line() +
+      labs(x = "Step", y = "Salary (thousands)", color = "Range")-> s
+    
+    s <- s + aes(text = paste("Range: ", Range, "<br>",
+                              "Step: ", Step, "<br>",
+                              "Salary: ", format(Salary, big.mark = ",", decimal.mark = ".", 
+                                                 nsmall = 2), "<br>"
+    ))
+    
+    ggplotly(s, tooltip = "text")
+})
+}
+
+shinyApp(ui = ui, server = server)
+
---
+++ code/scrapeSal.R
@@ -0,0 +1,50 @@
+library(rvest)
+library (httr)
+library(tidyverse)
+
+# func to create url given url_prefix and page num
+paste_url <- function (url_pre, pg){
+  paste0(url_pre, pg)
+}
+
+# func to extract first table from url 
+scrape_table <- function(url) {
+  html <- read_html(url)
+  table_nodes <- html_nodes(html, "table") # select all tables
+  table_nodes_1 <- table_nodes[1] 
+  table_1 <- html_table(table_nodes_1, header = TRUE, fill = TRUE)
+  table_df <- as.data.frame(table_1)
+  return(table_df)
+}
+
+url_pre <- "https://www.openthebooks.com/alaska-state-employees/?Year_S=2022&PG="
+p1 <- "1"
+num_pages <- 100 
+
+paste_url(url_pre, p1) -> url
+scrape_table(url) -> dat
+
+for (i in 2:num_pages) {
+  paste_url(url_pre, i) -> url
+  scrape_table(url) -> dat_i 
+  rbind(dat,dat_i) -> dat
+  cat("Page", i, "downloaded\n")
+}
+
+write.csv(dat, './data/sal_allDepts_22_10K.csv')
+
+#SUMMARIZE 
+# # 
+# dat %>% mutate(wages = as.numeric(gsub("\\$|,", "", Annual.Wages))) -> dat
+# dat %>% group_by (Title) %>% summarize (
+#     mean = mean(wages), 
+#     min = min(wages),
+#     max = max(wages),
+#     n = n()
+# ) -> summ
+# summ
+# 
+# write.csv(summ,"SAL_ADFG_22_sum.csv")
+
+
+
